29557eae8204a5319e4ab91673e94748255dd96c
HotBloodMan <18303011272@163.com>
2017-10-19 11:33:06 +0800

    Picture Cache

---
+++ app/src/main/java/com/ljt/photocachedemo/MainActivity.java
@@ -0,0 +1,126 @@
+package com.ljt.photocachedemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.GridView;
+
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+public static String TAG= MainActivity.class.getSimpleName();
+    /** 用于展示照片墙的GridView */
+    private GridView mPhotoWallView;
+    private int mImageThumbSize;
+    private int mImageThumbSpacing;
+
+    private static final String BASE_URL = "https://image.baidu.com/search/index?ct=201326592&cl=2&st=-1&lm=-1&nc=1&ie=utf-8&tn=baiduimage&ipn=r&rps=1&pv=&fm=rs8&word=";
+    private String[] mImageWords = { "小清新美女", "天空之城", "千与千寻", "清新美女", "美女壁纸" };
+    private static String url;
+    private static int page = 0;
+
+
+    private List<String> mImageUrlList = new ArrayList<>();
+
+    private ImageSource imageSource;
+
+    /** GridView的适配器 */
+    private PhotosWallAdapter mWallAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+        initEvent();
+        initData();
+        System.out.println("");
+    }
+
+    private void initView() {
+        Map<String,String> a=new HashMap<>();
+        mPhotoWallView = (GridView) findViewById(R.id.photo_wall);
+    }
+
+    private void initEvent() {
+        mImageThumbSize = getResources().getDimensionPixelSize(R.dimen.image_thumbnail_size);
+        mImageThumbSpacing = getResources().getDimensionPixelSize(R.dimen.image_thumbnail_spacing);
+        // 监听获取图片的宽高
+        mPhotoWallView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+
+            @Override
+            public void onGlobalLayout() {
+                // 计算列数
+                final int numColumns = (int) Math.floor(mPhotoWallView.getWidth() / (mImageThumbSize + mImageThumbSpacing));
+                if (numColumns > 0) {
+                    int columnWidth = (mPhotoWallView.getWidth() / numColumns) - mImageThumbSpacing;
+                    mWallAdapter.setItemSize(columnWidth);
+                    mPhotoWallView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                }
+            }
+        });
+    }
+
+    private void initData() {
+        if(mWallAdapter==null){
+            mWallAdapter=new PhotosWallAdapter(this,mImageUrlList,mPhotoWallView);
+        }
+        mPhotoWallView.setAdapter(mWallAdapter);
+        imageSource=new ImageSource();
+        getImageListFromNet(0);
+    }
+
+    private void getImageListFromNet(final int i) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                imageSource.setDuRegex(true);
+                String regex = ImageSource.regex[2];
+                String word = mImageWords[i];
+                url = BASE_URL + URLEncoder.encode(word);
+                 Log.d(TAG,TAG+" ----->>> url= "+url);
+                final ArrayList<String> images = imageSource.ParseHtmlToImage(url, regex);
+                Log.d(TAG,TAG+" ----->>> images= "+images);
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        mImageUrlList.addAll(images);
+                        mWallAdapter.notifyDataSetChanged();
+                    }
+                });
+            }
+        }).start();
+    }
+    public void add(View view){
+        page++;
+        if(page >= mImageWords.length){
+            page = page % mImageWords.length;
+        }
+        getImageListFromNet(page);
+    }
+
+    public void clear(View view){
+        mImageUrlList.clear();
+        mWallAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mWallAdapter.imageLoaders.flushCache();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // 退出程序时结束所有的下载任务
+        mWallAdapter.imageLoaders.cancelAllTasks();
+        mWallAdapter.imageLoaders.deleteCache();
+    }
+}
---
+++ app/src/main/java/com/ljt/photocachedemo/PhotosWallAdapter.java
@@ -0,0 +1,92 @@
+package com.ljt.photocachedemo;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by ${JT.L} on 2017/10/17.
+ */
+
+public class PhotosWallAdapter extends BaseAdapter {
+
+    public static String TAG= PhotosWallAdapter.class.getSimpleName();
+    private Context context;
+    private List<String> imageUrlList = new ArrayList<>();
+
+
+    /**
+     * 记录每个子项的高度
+     */
+    private int mItemHeight=0;
+
+    public ImageLoaders imageLoaders;
+
+    public PhotosWallAdapter(Context context, List<String> imageUrlList, AbsListView absListView) {
+        this.context = context;
+        this.imageUrlList = imageUrlList;
+        imageLoaders=new ImageLoaders(context,absListView);
+    }
+
+    @Override
+    public int getCount() {
+        if (imageUrlList != null) {
+            return imageUrlList.size();
+        }
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return imageUrlList.get(position);
+    }
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        String url=imageUrlList.get(position);
+         Log.d(TAG,TAG+" getView ----->>>url= "+url);
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(mItemHeight, mItemHeight);
+        ViewHolder holder;
+        if(convertView==null){
+            holder=new ViewHolder();
+            convertView= View.inflate(context, R.layout.photo_layout,null);
+            holder.iv= (ImageView) convertView.findViewById(R.id.photo);
+            holder.iv.setLayoutParams(layoutParams);
+            convertView.setTag(holder);
+        }else{
+            holder= (ViewHolder) convertView.getTag();
+        }
+        holder.iv.setTag(url);
+        //设置默认图片
+        holder.iv.setImageResource(R.drawable.empty_photo);
+        //加载数据
+        imageLoaders.loadBitmaps(holder.iv,url);
+        return convertView;
+    }
+
+
+    private class ViewHolder{
+        ImageView iv;
+    }
+
+    //设置Item子项的大小
+    public void setItemSize(int edgeLength){
+        mItemHeight=edgeLength;
+        imageLoaders.setItemLength(edgeLength);
+    }
+
+
+
+}
